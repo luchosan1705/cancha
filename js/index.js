010b35293574cae8fac6fc55a45141c64a54fa45
luciano santos <santosluciano1705@gmail.com>
2022-10-31 20:32:31 -0300

    add defense

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 document.getElementById('arquero').addEventListener('click', function(e){
         e.target.classList.toggle('selected');
 });
+document.getElementById('central').addEventListener('click', function(e){
+        e.target.classList.toggle('selected');
+});
+document.getElementById('lateral-derecho').addEventListener('click', function(e){
+        e.target.classList.toggle('selected');
+});
+document.getElementById('lateral-izquierdo').addEventListener('click', function(e){
+        e.target.classList.toggle('selected');
+});
