3fabb878f16e0c3b47f70df037d336df6352d3f6
luciano santos <santosluciano1705@gmail.com>
2022-10-31 20:50:31 -0300

    add midfielders

--- js/index.js
+++ js/index.js
@@ -10,3 +10,15 @@ document.getElementById('lateral-derecho').addEventListener('click', function(e)
 document.getElementById('lateral-izquierdo').addEventListener('click', function(e){
         e.target.classList.toggle('selected');
 });
+document.getElementById('volante-central-izquierdo').addEventListener('click', function(e){
+        e.target.classList.toggle('selected');
+});
+document.getElementById('volante-izquierdo').addEventListener('click', function(e){
+        e.target.classList.toggle('selected');
+});
+document.getElementById('volante-derecho').addEventListener('click', function(e){
+        e.target.classList.toggle('selected');
+});
+document.getElementById('volante-central-derecho').addEventListener('click', function(e){
+        e.target.classList.toggle('selected');
+});
